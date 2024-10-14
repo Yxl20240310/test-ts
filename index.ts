e7eee82f0490f3fc3a54007b09013a7595c624d5
yuxuelian.123 <yuxuelian.123@bytedance.com>
2024-10-14 17:18:04 +0800

    更新

--- index.ts
+++ index.ts
@@ -1,29 +1,55 @@
-console.log('Hello, 大雪连!');
-console.log('Hello, 大雪连!')
+// // FILEPATH: /Users/bytedance/Documents/projects/testNativeIDE/test-ts/index.ts
 
-function quickSort(arr: number[]): number[] {
-    if (arr.length <= 1) {
-        return arr;
-    }
+// import http from 'http';
 
-    const pivot = arr[Math.floor(arr.length / 2)];
-    const left = [];
-    const right = [];
-    const equal = [];
+// const PORT = 3000; // 指定服务器监听的端口
 
-    for (const element of arr) {
-        if (element < pivot) {
-            left.push(element);
-        } else if (element > pivot) {
-            right.push(element);
-        } else {
-            equal.push(element);
-        }
-    }
+// // 创建HTTP服务器
+// const server = http.createServer((req, res) => {
+//     // 设置响应头
+//     res.writeHead(200, { 'Content-Type': 'text/plain' });
+//     // 发送响应内容
+//     res.end('Hello, Wovvvrld!\n');
+// });
 
-    return [...quickSort(left), ...equal, ...quickSort(right)];
-}
+// // 服务器开始监听指定端口
+// server.listen(PORT, () => {
+//     console.log(`Server is running on http://localhost:${PORT}`);
+// });
 
-// 示例使用
-const arr = [3, 6, 8, 10, 1, 2, 1];
-console.log(quickSort(arr))// 输出: [1, 1, 2, 3, 6, 8, 10]
+
+
+
+//TODO 
+// FILEPATH: /Users/bytedance/Documents/projects/testNativeIDE/test-ts/index.ts
+
+import http from 'http';
+
+const PORT1 = 3000; // 第一个服务器监听的端口
+const PORT2 = 3001; // 第二个服务器监听的端口
+
+// 创建第一个HTTP服务器
+const server1 = http.createServer((req, res) => {
+    // 设置响应头
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    // 发送响应内容
+    res.end(`hello,world Connected to port ${PORT1}\n`);
+});
+
+// 创建第二个HTTP服务器
+const server2 = http.createServer((req, res) => {
+    // 设置响应头
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    // 发送响应内容
+    res.end(`Connected to port ${PORT2}\n`);
+});
+
+// 第一个服务器开始监听指定端口
+server1.listen(PORT1, () => {
+    console.log(`Server1 is running on http://localhost:${PORT1}`);
+});
+
+// 第二个服务器开始监听指定端口
+server2.listen(PORT2, () => {
+    console.log(`Server2 is running on http://localhost:${PORT2}`);
+});
